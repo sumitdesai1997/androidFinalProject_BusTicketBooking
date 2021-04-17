ed8fbe17d22b3ddc21fb8665b0cc1a90c8ffdcde
Sumit <sumitdesai1997@gmail.com>
2021-04-17 11:32:20 +0530

    Logic for login page done and deisgn for sign up done

--- app/src/main/java/com/example/androidfinalproject_busbooking/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject_busbooking/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.androidfinalproject_busbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgLogo;
+    EditText etEmail, etPassword;
+    Button btnSignIn, btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgLogo = findViewById(R.id.imgLogo);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        int imgLogoId = getResources().getIdentifier("buslogo5","mipmap",getPackageName());
+        imgLogo.setImageResource(imgLogoId);
+
+        btnSignIn.setOnClickListener(new ButtonEvents());
+        btnCreateAccount.setOnClickListener(new ButtonEvents());
+    }
+
+    private class ButtonEvents implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.btnSignIn) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                String emailReg = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+                String passReg = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
+
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getBaseContext(), " Please enter value for required fields", Toast.LENGTH_SHORT).show();
+                } else if (!Pattern.matches(emailReg,email)){
+                    Toast.makeText(getBaseContext(), " Please enter valid email id", Toast.LENGTH_SHORT).show();
+                } else if (!Pattern.matches(passReg,password)){
+                    Toast.makeText(getBaseContext(), " Please enter at least 8 character that has at least 1 digit, 1 uppercase and 1 lowercase letter", Toast.LENGTH_SHORT).show();
+                } else {
+                   Intent intent = new Intent(getBaseContext(), SignUp.class);
+                   startActivity(intent);
+                }
+            } else {
+
+            }
+        }
     }
 }
